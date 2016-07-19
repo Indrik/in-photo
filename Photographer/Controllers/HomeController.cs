c2816aeacc748ef122fa86c388ebde454709e062
Indrik <kaymanov.as@yandex.ru>
2016-07-19 08:03:19 +0300

    correct contacts

--- Photographer/Controllers/HomeController.cs
+++ Photographer/Controllers/HomeController.cs
@@ -8,19 +8,5 @@ namespace Photographer.Controllers
         {
             return View();
         }
-
-        public ActionResult Portfolio()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
