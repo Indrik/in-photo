8b83ea8b03d2a2c8e4d266b8f952e8f4b9e01d52
Indrik <kaymanov.as@yandex.ru>
2016-07-19 07:53:44 +0300

    split pages on controllers

---
+++ Photographer/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Photographer.Controllers
+{
+    public class ContactController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
