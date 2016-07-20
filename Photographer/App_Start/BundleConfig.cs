16733d763a30e9f16979dcaa66a9c018a0f1f8d9
Indrik <kaymanov.as@yandex.ru>
2016-07-20 23:00:25 +0300

    add fancybox scripts for main page

--- Photographer/App_Start/BundleConfig.cs
+++ Photographer/App_Start/BundleConfig.cs
@@ -24,7 +24,12 @@ namespace Photographer
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/jquery.fancybox.css"));
+
+            bundles.Add(new ScriptBundle("~/fancyboxJs").Include(
+                "~/Scripts/custom/jquery.fancybox.pack.js"
+                ));
         }
     }
 }
