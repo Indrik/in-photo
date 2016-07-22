02ef06552f7ab74b76d5709128b8dea6dd458864
Kaymanov Andrey <kaymanov@moedelo.org>
2016-07-22 16:51:58 +0300

    install LightInject.Mvc

--- Photographer/Controllers/HomeController.cs
+++ Photographer/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using System.Web.Mvc;
+using Photographer.Contract;
 
 namespace Photographer.Controllers
 {
     public class HomeController : Controller
     {
+        public IPhotoGalleryHandler PhotoGalleryHandler { get; set; }
+
         public ActionResult Index()
         {
             return View();
