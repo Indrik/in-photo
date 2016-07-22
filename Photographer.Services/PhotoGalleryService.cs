1f1a03d219f525628954474d504bb69f82d90e93
Kaymanov Andrey <kaymanov@moedelo.org>
2016-07-22 15:26:47 +0300

    add libraries structure

--- Photographer.Services/PhotoGalleryService.cs
+++ Photographer.Services/PhotoGalleryService.cs
@@ -1,6 +1,12 @@
-﻿namespace Photographer.Services
+﻿using System.Collections.Generic;
+
+namespace Photographer.Services
 {
     public class PhotoGalleryService
     {
+        public List<string> GetGalleryFileNames(string galleryName)
+        {
+            return new List<string>();
+        }
     }
 }
