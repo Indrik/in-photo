ac9f0cd7029fc3d2987e452fc610fdac5b42813c
Kaymanov Andrey <kaymanov@moedelo.org>
2016-07-22 18:14:45 +0300

    begin write service registration

---
+++ Photographer/App_Start/ServiceInstaller.cs
@@ -0,0 +1,33 @@
+﻿using LightInject;
+
+namespace Photographer
+{
+    public class ServiceInstaller
+    {
+        private readonly ServiceContainer container;
+
+        public ServiceInstaller(ServiceContainer container)
+        {
+            this.container = container;
+        }
+
+        private bool Initialized { get; set; }
+
+        public void Initialize()
+        {
+            if (Initialized)
+            {
+                return;
+            }
+
+            RegisterBehaviour();
+            Initialized = true;
+        }
+
+        private void RegisterBehaviour()
+        {
+            container.RegisterControllers();
+            container.EnableMvc();
+        }
+    }
+}
